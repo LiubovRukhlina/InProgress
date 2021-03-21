9aa6b05fa48b791f71f1db7de88a31880c48a513
AshChamp-dot <80144076+AshChamp-dot@users.noreply.github.com>
2021-03-21 20:57:16 +0530

    Update Iron.java

--- AsteroidMining/Asteroid Mining/src/com/InProgress/Iron.java
+++ AsteroidMining/Asteroid Mining/src/com/InProgress/Iron.java
@@ -1,7 +1,11 @@
 package com.InProgress;
 
-public class Iron implements ResourceBase { //@Ash is there a reason why it is implements and not extends?
+public class Iron extends ResourceBase { 
     /**
      * resource iron
      */
+ public Iron(String resourceType) {
+        super(resourceType);
+    }
 }
+
