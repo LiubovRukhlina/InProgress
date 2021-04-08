00bba5cbb3febf626f2d33b5edba50753279ec77
AshChamp-dot <80144076+AshChamp-dot@users.noreply.github.com>
2021-04-08 15:56:29 +0530

    Update Robot.java

--- AsteroidMining/Asteroid Mining/src/com/InProgress/Robot.java
+++ AsteroidMining/Asteroid Mining/src/com/InProgress/Robot.java
@@ -1,12 +1,12 @@
 package com.InProgress;
 
-public class Robot extends TravellerBase{
+public class Robot extends TravellerBase {
 
     //<editor-fold desc="Attributes">
     /**
      * currentX gives the x coordinate of the current asteroid of the robot
      * currentY gives the y coordinate of the current asteroid of the robot
-     *
+     * <p>
      * currentZ gives the Z coordinate of the current asteroid of the robot
      */
     private int damageCount;
@@ -28,14 +28,11 @@ public class Robot extends TravellerBase{
         this.damageCount = 0;
     }
 
-    public int getDamageCount() { return damageCount; }
-    public void setDamageCount(int damageCount) { this.damageCount = damageCount; }
 
     /**
-     *
      * @param Dest destination asteroid where the user wants to travel
      */
-    public void travel(Asteroid Dest){
+    public void travel(Asteroid Dest) {
         int DestX = Dest.getX();
         int DestY = Dest.getY();
         int DestZ = Dest.getZ();
@@ -43,7 +40,7 @@ public class Robot extends TravellerBase{
         /**
          * According to the guidelines (check and revert)
          */
-        if(Maths.abs(DestX-currentX) <= 2 || Maths.abs(DestY-currentY) <= 2 || Maths.abs(DestZ-currentZ) <= 2 )
+        if (Maths.abs(DestX - currentX) <= 2 || Maths.abs(DestY - currentY) <= 2 || Maths.abs(DestZ - currentZ) <= 2)
             currentLocation = Dest;
 
     }
@@ -51,17 +48,16 @@ public class Robot extends TravellerBase{
     /**
      * Allows users to fast travel
      */
-    public void fastTravel(){
-        if(currentLocation.getHasGate()){ // if the current asteroid has a transport gate
+    public void fastTravel() {
+        if (currentLocation.getHasGate()) { // if the current asteroid has a transport gate
             TransportGate Gate1 = currentLocation.getGate();
 
-            if(Gate1.isActive){    // if the gate is active (means if the pair is also deployed)
+            if (Gate1.isActive) {    // if the gate is active (means if the pair is also deployed)
                 TransportGate Gate2 = Gate1.getPair();
                 currentLocation = Gate2.getCurrentPosition(); // we travel to the location of the pair.
             }
         }
     }
-
 
 
     //</editor-fold>
@@ -78,8 +74,7 @@ public class Robot extends TravellerBase{
         System.out.println("increaseDamage()");
         damageCount++;
 
-        if(this.damageCount == 2)
-        {
+        if (this.damageCount == 2) {
             this.die();
         }
 
@@ -90,9 +85,7 @@ public class Robot extends TravellerBase{
     //</editor-fold>
 
 
-
     //<editor-fold desc="Getters and Setters">
-
 
 
     /**
@@ -101,7 +94,7 @@ public class Robot extends TravellerBase{
     public void drill() {
 
         int depth = currentLocation.getDepth(); // gets the mantle length of the asteroid
-        if(depth != 0) { // if it is not hollow, then we drill
+        if (depth != 0) { // if it is not hollow, then we drill
             for (int i = 0; i < depth; i++) {
                 currentLocation.decreaseRockCover(); //drilling
             }
@@ -111,13 +104,21 @@ public class Robot extends TravellerBase{
     /**
      * hide method
      */
-    public void hide{
-        if(!isHidden){ //if the robot is already hidden
-            if(currentLocation.IsHollow){ // if the asteroid is a hollow one
+    public void hide() {
+        if (!isHidden) { //if the robot is already hidden
+            if (currentLocation.IsHollow) { // if the asteroid is a hollow one
                 isHidden = true;
             }
         }
 
+    }
+
+    public int getDamageCount() {
+        return damageCount;
+    }
+
+    public void setDamageCount(int damageCount) {
+        this.damageCount = damageCount;
     }
 
 
