51a21356f878775e3d6422adfca450e7666879d7
LiubovRukhlina <36957053+LiubovRukhlina@users.noreply.github.com>
2021-05-06 16:22:19 +0200

    Supportive GUI classes

--- AsteroidMining/Asteroid Mining/src/com/InProgress/Main.java
+++ AsteroidMining/Asteroid Mining/src/com/InProgress/Main.java
@@ -11,9 +11,24 @@ public class Main {
     // TODO Final implementation
     public static void main(String[] args) throws IOException {
 
-        GameWindow gameWindow = new GameWindow();
+       // GameWindow gameWindow = new GameWindow();
+//        BuildWindow buildWindow = new BuildWindow();
+//        buildWindow.initialize();
 
-
+//        StartWindow startWindow = new StartWindow();
+//        startWindow.initialize();
+//
+//        TravelWindow travelWindow = new TravelWindow();
+//        travelWindow.initialize();
+//
+//        ErrorMessage errorMessage = new ErrorMessage();
+//        errorMessage.initialize();
+//
+//        MineMessage mineMessage = new MineMessage();
+//        mineMessage.initialize();
+//
+//        LeaveResourcesWindow leaveResourcesWindow = new LeaveResourcesWindow();
+//        leaveResourcesWindow.initialize();
 
         while (true) {
 
